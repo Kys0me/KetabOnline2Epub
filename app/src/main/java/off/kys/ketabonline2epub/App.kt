548cbabdbf652119a4c6734e78494c8a6d78ca6a
kys0ff <idk683120@gmail.com>
2026-01-30 09:50:08 +0100

    feat: Clear cache on application startup

--- app/src/main/java/off/kys/ketabonline2epub/App.kt
+++ app/src/main/java/off/kys/ketabonline2epub/App.kt
@@ -10,6 +10,9 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        // Clear app cache and start up
+        cacheDir.listFiles()?.forEach { it.deleteRecursively() }
+
         startKoin {
             androidContext(this@App)
             modules(appModule)
